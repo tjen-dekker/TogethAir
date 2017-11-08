cbfed33a64b13b6a7e37683e343eeef19fdc8b30
Gust Wittevrongel <gust.wittevrongel@realdolmen.com>
2017-11-08 19:38:45 +0100

    methods implemented

--- src/main/java/com/realdolmen/togethair/beans/SearchBean.java
+++ src/main/java/com/realdolmen/togethair/beans/SearchBean.java
@@ -1,10 +1,15 @@
 package com.realdolmen.togethair.beans;
 
+import com.realdolmen.togethair.DTO.FlightDTO;
+import com.realdolmen.togethair.domain.Flight;
+import com.realdolmen.togethair.domain.TravelClass;
 import com.realdolmen.togethair.services.SearchServiceBean;
 
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by GWTBF10 on 8/11/2017.
@@ -15,5 +20,23 @@ public class SearchBean {
 	@Inject
 	private SearchServiceBean searchService;
 	
+	public List<FlightDTO> findFromToOnDate(String fromCityName, String toCityName, Date date){
+		return searchService.findFromToOnDate(fromCityName,toCityName,date);
+	}
 	
+	public List<FlightDTO> findFromTo(String fromCityName, String toCityName){
+		return searchService.findFromTo(fromCityName,toCityName);
+	}
+	
+	public List<FlightDTO> findFromToSortedByDate(String fromCityName, String toCityName){
+		return searchService.findFromToSortedByDate(fromCityName,toCityName);
+	}
+	
+	public List<FlightDTO> findFromTo(String fromCityName, String toCityName, TravelClass travelClass, int minNrOfSeats){
+		return searchService.findFromTo(fromCityName,toCityName,travelClass,minNrOfSeats);
+	}
+	
+	public List<FlightDTO> findFromToSortedByDate(String fromCityName, String toCityName, TravelClass travelClass, int minNrOfSeats){
+		return searchService.findFromToSortedByDate(fromCityName,toCityName,travelClass,minNrOfSeats);
+	}
 }
