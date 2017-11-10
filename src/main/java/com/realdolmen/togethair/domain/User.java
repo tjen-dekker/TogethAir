edfb24e3ee6b8fc4ea5ddd1764182bf782491891
Gust Wittevrongel <gust.wittevrongel@realdolmen.com>
2017-11-10 13:09:38 +0100

    added pricecalculation service and method to calculate totalprice added volume discounts and override of price to flight added getter and setter for user to master added resoucre bundle for price calculation properties

--- src/main/java/com/realdolmen/togethair/domain/User.java
+++ src/main/java/com/realdolmen/togethair/domain/User.java
@@ -36,7 +36,15 @@ public abstract class User {
 	@Column(length = 80)
 	@Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])")
 	private String email;
-
+	
+	public List<Booking> getBookings() {
+		return bookings;
+	}
+	
+	public void setBookings(List<Booking> bookings) {
+		this.bookings = bookings;
+	}
+	
 	public String getFirstName() {return firstName;}
 
 	public void setFirstName(String firstName) {this.firstName = firstName;}
