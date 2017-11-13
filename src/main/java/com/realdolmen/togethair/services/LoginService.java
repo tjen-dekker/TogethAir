8245cc1f686ba0dbb4fcc945c0e1e28eb61a98d1
Tjen Dekker <Tjen.Dekker@realdolmen.com>
2017-11-13 09:48:15 +0100

    incorporated registering into loginservice for bean purposes

--- src/main/java/com/realdolmen/togethair/services/LoginService.java
+++ src/main/java/com/realdolmen/togethair/services/LoginService.java
@@ -1,9 +1,13 @@
 package com.realdolmen.togethair.services;
 
 
+import com.realdolmen.togethair.domain.Role;
+import com.realdolmen.togethair.domain.User;
+import com.realdolmen.togethair.repositories.RolesRepository;
 import com.realdolmen.togethair.repositories.UserRepository;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
+import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,13 +28,25 @@ public class LoginService implements Serializable {
     @Inject
     private UserRepository userRepository;
 
+    @Inject
+    private RolesRepository rolesRepository;
+
     private String username;
     private String password;
     private String firstName;
     private String lastName;
+    private String hashedPassword;
+    private boolean isAdmin;
     private Boolean rememberMe;
 
     public LoginService() {
+        if(SecurityUtils.getSubject().isAuthenticated()){
+            username = SecurityUtils.getSubject().getPrincipal().toString();
+            firstName = userRepository.getFirstNameofCurrentUser(username);
+            lastName = userRepository.getLastNameofCurrentUser(username);
+        }
+
+
     }
 
     /**
@@ -69,12 +85,63 @@ public class LoginService implements Serializable {
 
 
             token.clear();
-
-
         }
 
-        setFirstName(userRepository.getFirstNameofCurrentUser(username));
-        setLastName(userRepository.getLastNameofCurrentUser(username));
+    }
+
+    private void generatePassword(User user, String plainTextPassword) {
+//        RandomNumberGenerator rng = new SecureRandomNumberGenerator();
+//        Object salt = rng.nextBytes();
+        // Now hash the plain-text password with the random salt and multiple
+        // iterations and then Base64-encode the value (requires less space than
+        // Hex):
+
+        if (plainTextPassword.length() > 5) {
+            setHashedPassword(new Sha256Hash(plainTextPassword).toBase64());
+
+            user.setPassword(hashedPassword);
+//        user.setSalt(salt.toString());
+        } else throw new IncorrectCredentialsException();
+
+    }
+
+
+    public void register() {
+        User user = new User();
+
+        user.setEmail(username);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+
+
+        try {
+            generatePassword(user, password);
+
+            System.err.println("User with email:" + user.getEmail()
+                    + " hashedPassword:" + user.getPassword() + " salt:"
+                    + user.getSalt());
+
+            if (isAdmin) {
+                Role role = new Role();
+                role.setEmail(username);
+                role.setRoleName("admin");
+                rolesRepository.create(role);
+            }
+
+            userRepository.create(user);
+
+
+            setRememberMe(false);
+
+            doLogin();
+
+
+
+        } catch (IncorrectCredentialsException ex) {
+            facesError("password needs to be at least 5 characters long");
+        }
+
+
     }
 
     public void logout() {
@@ -135,5 +202,21 @@ public class LoginService implements Serializable {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
     }
 }
