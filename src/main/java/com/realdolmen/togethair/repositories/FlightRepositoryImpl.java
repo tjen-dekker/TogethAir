548e7d71821bc45ab3b19c2afd01ca64b54c6acc
Gust Wittevrongel <gust.wittevrongel@realdolmen.com>
2017-11-13 15:15:38 +0100

    cleaned up unused methods

--- src/main/java/com/realdolmen/togethair/repositories/FlightRepositoryImpl.java
+++ src/main/java/com/realdolmen/togethair/repositories/FlightRepositoryImpl.java
@@ -36,19 +36,6 @@ public class FlightRepositoryImpl implements FlightRepository{
                 .getSingleResult();
     }
 	
-    public List<Flight> findFromToOnDate(List<Airport> from, List<Airport> to,Date date){
-		LocalDate localDate = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()).toLocalDate();
-		List<Flight> tempList=  findFromTo(from,to);
-		
-		List<Flight> resultList = new ArrayList<>();
-		for(Flight f : tempList){
-			LocalDate localDateOfFlight = LocalDateTime.ofInstant(f.getDepartureDateTime().toInstant(), ZoneId.systemDefault()).toLocalDate();
-			if(localDateOfFlight.isEqual(localDate))
-				resultList.add(f);
-		}
-		return resultList;
-	}
-	
 	public List<Flight> findFromToBetweenDates(List<Airport> from, List<Airport> to,Date date1, Date date2){
 		LocalDate localDate1 = LocalDateTime.ofInstant(date1.toInstant(), ZoneId.systemDefault()).toLocalDate();
 		LocalDate localDate2 = LocalDateTime.ofInstant(date2.toInstant(), ZoneId.systemDefault()).toLocalDate();
